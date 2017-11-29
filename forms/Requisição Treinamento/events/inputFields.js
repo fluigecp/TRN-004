2cbe7e7ba2be1b712ced83c4636e1d5d9120e688
Lucas Pimenta <lucas.pimenta94@outlook.com>
2017-11-29 17:10:53 +0000

    ANALYTICS: Adicionando e tratando dados

--- forms/Requisição Treinamento/events/inputFields.js
+++ forms/Requisição Treinamento/events/inputFields.js
@@ -7,7 +7,33 @@ function inputFields(form) {
     /** Life Cycle */
     if ( activity == 0 || activity == 4 ) {
         form.setValue("numProcess", numProcess);
-    }
+	}
+	
+	form.setValue('custom_0', form.getValue('treinamentoSolicitado') );
+	form.setValue('custom_1', form.getValue('departamento') );
+	form.setValue('custom_2', getClassificacaoCameoCase( form.getValue('classificacaoCurso') ) );
+	form.setValue('custom_3', form.getValue('cargaHorariaEstimada') );
+	form.setValue('custom_4', form.getValue('anoVigencia') );
+	form.setValue('custom_5', form.getValue('totalParticipantes') );
+
+	form.setValue('fato_0', getInvestimentoParticipante( form.getValue('valorUtilizado'), form.getValue('totalParticipantes') ) );
+	form.setValue('fato_1', converteParaFloat( form.getValue('valorEstimado') ) );
+	form.setValue('fato_2', converteParaFloat( form.getValue('valorUtilizado') ) );
+}
+
+function getClassificacaoCameoCase(classificacao) {
+	if ( classificacao == "legislacao_obrigatorio" ) return "Legislação/obrigatório";
+	if ( classificacao == "projeto_implantacao" ) return "Projeto/implantação";
+	if ( classificacao == "aprimoramento_profissional" ) return "Aprimoramento Profissional";
+	return "";
+}
+
+function getInvestimentoParticipante(valorInvestido, qtdParticipante ) {
+	if ( qtdParticipante != "" && valorInvestido != "" ) {
+		var valorParticipante = converteParaFloat(valorInvestido) / parseFloat(qtdParticipante);
+		return valorParticipante;
+	}
+	return 0;
 }
 
 function converteParaFloat(variavel) {
